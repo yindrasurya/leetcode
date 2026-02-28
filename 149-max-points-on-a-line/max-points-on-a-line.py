e59a5ea365e853ab8a7be7711de9f7db143d96bd
yindrasurya <starsurya933@gmail.com>
2026-03-01 00:51:25 +0530

    Time: 2287 ms (5.01%) | Memory: 22.4 MB (17.67%) - LeetSync

---
+++ 149-max-points-on-a-line/max-points-on-a-line.py
@@ -0,0 +1,18 @@
+class Solution(object):
+    def maxPoints(self, points):
+        """
+        :type points: List[List[int]]
+        :rtype: int
+        """
+        ll = len(points) 
+        if ll == 1: return 1
+        elif ll == 2: return 2
+        else:
+            ans = 0
+            for i in list(combinations(points,2)):
+                cnt = 0
+                for j in points:
+                    if (j[1] - i[0][1]) * (i[1][0] - i[0][0]) == (j[0] - i[0][0]) * (i[1][1] - i[0][1]):
+                        cnt += 1
+                ans = max(ans, cnt)
+            return ans

d7e1c1ef7c26795afea14f9f1fda748953d2c2c9
yindrasurya <starsurya933@gmail.com>
2026-03-08 23:38:55 +0530

    Time: 329 ms (80.53%) | Memory: 29.1 MB (30.78%) - LeetSync

--- 313-super-ugly-number/super-ugly-number.py
+++ 313-super-ugly-number/super-ugly-number.py
@@ -12,9 +12,7 @@ class Solution:
             if next_ugly not in seen:
                 ugly_numbers.append(next_ugly)
                 seen.add(next_ugly)
-            
-            # to make the size of the heap doesn't increase more than
-            # the length of the heap
+
             while next_ugly in seen:
                 prime_index += 1
                 next_ugly = ugly_numbers[prime_index] * prime_value

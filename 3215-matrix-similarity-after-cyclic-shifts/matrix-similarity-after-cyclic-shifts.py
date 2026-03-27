f8ba98238e16d053c4f3b2e8a65f2a91c346cd01
yindrasurya <starsurya933@gmail.com>
2026-03-28 01:31:08 +0530

    Time: 0 ms (100.00%) | Memory: 19.3 MB (90.11%) - LeetSync

---
+++ 3215-matrix-similarity-after-cyclic-shifts/matrix-similarity-after-cyclic-shifts.py
@@ -0,0 +1,15 @@
+class Solution:
+    def areSimilar(self, mat, k):
+        m, n = len(mat), len(mat[0])
+        k %= n
+
+        for i in range(m):
+            for j in range(n):
+                if i % 2 == 0:
+                    new_col = (j + k) % n
+                else:
+                    new_col = (j - k) % n
+
+                if mat[i][j] != mat[i][new_col]:
+                    return False
+        return True
